6d0c292d8ff9a80e438c528b192826b2f5c19d5d
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2021-05-08 16:19:47 +0200

    added postgres database configurationn

--- config/database.php
+++ config/database.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url('postgres://ulhptnnsbaqznv:4f95d3701d26a2f7686747da5fc438bc88d7677ee7d1f4d409772775cd1ecd22@ec2-34-206-8-52.compute-1.amazona
-ws.com:5432/d8von0b1hp4n8b');
-
 return [
 
     /*
@@ -71,16 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
 //            'url' => env('DATABASE_URL'),
-//            'host' => env('DB_HOST', '127.0.0.1'),
-//            'port' => env('DB_PORT', '5432'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'host' => env('DB_HOST', 'ec2-34-206-8-52.compute-1.amazonaws.com'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'd8von0b1hp4n8b'),
+            'username' => env('DB_USERNAME', 'ulhptnnsbaqznv'),
+            'password' => env('DB_PASSWORD', '4f95d3701d26a2f7686747da5fc438bc88d7677ee7d1f4d409772775cd1ecd22'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
